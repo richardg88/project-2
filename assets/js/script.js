86719f80c6047608b82cee67e6cb7ae418a7125c
richardg88 <richard_grosvenor@hotmail.com>
2022-05-31 19:07:19 +0000

    Add game function

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,14 +8,48 @@ const rockDiv = document.getElementById("rock");
 const paperDiv = document.getElementById("paper");
 const scissorsDiv = document.getElementById("scissors");
 
-rockDiv.addEventListener('click', function() {
-    console.log("You Chose Rock");
-})
+/* The code below adds event listeners to the
+image choices when clicked
+*/
+function getComputerChoice(){
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices [randomNumber];
+}
 
-paperDiv.addEventListener('click', function() {
-    console.log("You Chose Paper");
-})
+function game(userChoice) {
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice) {
+        case "rockpaper":
+        case "paperrock":
+        case "scissorspaper":
+            console.log("User Wins");
+            break;
+        case "rockpaper":
+        case "paperscissors":
+        case "scissorspaper":
+            console.log("User Loses");
+            break;
+        case "rockrock":
+        case "paperpaper":
+        case "scissorsscissors":
+            console.log("Its a Draw");
+            break;                                         
+    } 
+}
 
-scissorsDiv.addEventListener('click', function() {
-    console.log("You Chose Scissors");
-})
+function main(){
+  rockDiv.addEventListener('click', function() {
+    game("rock");
+  })
+
+  paperDiv.addEventListener('click', function() {
+    game("paper");
+  })
+
+  scissorsDiv.addEventListener('click', function() {
+    game("scissors");
+  })
+}
+
+main();
